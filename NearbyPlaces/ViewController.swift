2a4e120f4bfc63d21e75209760b470715d8dd6e7
Kevin Joseph Mangulabnan <wattholm@users.noreply.github.com>
2020-01-11 13:56:03 +0800

    Initial Commit

---
+++ NearbyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearbyPlaces
+//
+//  Created by KJEM on 11/01/2020.
+//  Copyright © 2020 KJEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
